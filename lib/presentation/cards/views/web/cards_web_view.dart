a924ec395da61176c1e7e9eb4be08e8b5b1940c1
sherifhasan <sherif.hasan1994@gmail.com>
2023-07-15 19:08:09 +0200

    [Fix]: fix remove card when it's last item

--- lib/presentation/cards/views/web/cards_web_view.dart
+++ lib/presentation/cards/views/web/cards_web_view.dart
@@ -71,144 +71,157 @@ class CardsWebView extends HookConsumerWidget {
                     SizedBox(
                       height: MediaQuery.sizeOf(context).height * 0.80,
                       width: MediaQuery.sizeOf(context).width * 0.3,
-                      child: StackedCardCarousel(
-                        pageController: cardsController,
-                        type: StackedCardCarouselType.cardsStack,
-                        spaceBetweenItems:
-                            MediaQuery.sizeOf(context).height * 0.65,
-                        items: cards
-                            .map((card) => Card(
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(18)),
-                                  elevation: 4,
-                                  child: Container(
-                                    width:
-                                        MediaQuery.sizeOf(context).width * 0.3,
-                                    height: MediaQuery.sizeOf(context).height *
-                                        0.63,
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(18),
-                                        image: const DecorationImage(
-                                            image: AssetImage(
-                                                'assets/images/card_bg.png'),
-                                            fit: BoxFit.fill)),
-                                    child: Stack(
-                                      children: [
-                                        Padding(
-                                          padding: const EdgeInsets.all(30),
-                                          child: Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  card.fullName,
-                                                  style: GoogleFonts.poppins(
-                                                      fontSize: 24,
-                                                      fontWeight:
-                                                          FontWeight.w600,
-                                                      color: const Color(
-                                                          0xFFF3F5F7)),
-                                                ),
-                                                Text(
-                                                  card.phoneNumber,
-                                                  style: GoogleFonts.poppins(
-                                                      fontSize: 14,
-                                                      fontWeight:
-                                                          FontWeight.w600,
-                                                      color: const Color(
-                                                          0xccF3F5F7)),
-                                                ),
-                                                Text(
-                                                  card.email,
-                                                  style: GoogleFonts.poppins(
-                                                      fontSize: 10,
-                                                      fontWeight:
-                                                          FontWeight.w600,
-                                                      color: const Color(
-                                                          0xccF3F5F7)),
-                                                ),
-                                              ]),
-                                        ),
-                                        Positioned(
-                                            left: 30,
-                                            right: 30,
-                                            bottom: 72,
-                                            child: Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  'IBAN',
-                                                  style: GoogleFonts.poppins(
-                                                      fontSize: 16,
-                                                      fontWeight:
-                                                          FontWeight.w600,
-                                                      color: const Color(
-                                                          0xFFF3F5F7)),
-                                                ),
-                                                Row(
-                                                  children: [
-                                                    Expanded(
-                                                      child: Text(
-                                                        card.iban,
+                      child: cards.isNotEmpty
+                          ? StackedCardCarousel(
+                              pageController: cardsController,
+                              type: StackedCardCarouselType.cardsStack,
+                              spaceBetweenItems:
+                                  MediaQuery.sizeOf(context).height * 0.65,
+                              items: cards
+                                  .map((card) => Card(
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(18)),
+                                        elevation: 4,
+                                        child: Container(
+                                          width:
+                                              MediaQuery.sizeOf(context).width *
+                                                  0.3,
+                                          height: MediaQuery.sizeOf(context)
+                                                  .height *
+                                              0.63,
+                                          decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(18),
+                                              image: const DecorationImage(
+                                                  image: AssetImage(
+                                                      'assets/images/card_bg.png'),
+                                                  fit: BoxFit.fill)),
+                                          child: Stack(
+                                            children: [
+                                              Padding(
+                                                padding:
+                                                    const EdgeInsets.all(30),
+                                                child: Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    children: [
+                                                      Text(
+                                                        card.fullName,
+                                                        style: GoogleFonts.poppins(
+                                                            fontSize: 24,
+                                                            fontWeight:
+                                                                FontWeight.w600,
+                                                            color: const Color(
+                                                                0xFFF3F5F7)),
+                                                      ),
+                                                      Text(
+                                                        card.phoneNumber,
+                                                        style: GoogleFonts.poppins(
+                                                            fontSize: 14,
+                                                            fontWeight:
+                                                                FontWeight.w600,
+                                                            color: const Color(
+                                                                0xccF3F5F7)),
+                                                      ),
+                                                      Text(
+                                                        card.email,
+                                                        style: GoogleFonts.poppins(
+                                                            fontSize: 10,
+                                                            fontWeight:
+                                                                FontWeight.w600,
+                                                            color: const Color(
+                                                                0xccF3F5F7)),
+                                                      ),
+                                                    ]),
+                                              ),
+                                              Positioned(
+                                                  left: 30,
+                                                  right: 30,
+                                                  bottom: 72,
+                                                  child: Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    children: [
+                                                      Text(
+                                                        'IBAN',
                                                         style: GoogleFonts.poppins(
                                                             fontSize: 16,
                                                             fontWeight:
                                                                 FontWeight.w600,
                                                             color: const Color(
-                                                                0xffF3F5F7)),
+                                                                0xFFF3F5F7)),
                                                       ),
+                                                      Row(
+                                                        children: [
+                                                          Expanded(
+                                                            child: Text(
+                                                              card.iban,
+                                                              style: GoogleFonts.poppins(
+                                                                  fontSize: 16,
+                                                                  fontWeight:
+                                                                      FontWeight
+                                                                          .w600,
+                                                                  color: const Color(
+                                                                      0xffF3F5F7)),
+                                                            ),
+                                                          ),
+                                                        ],
+                                                      ),
+                                                    ],
+                                                  )),
+                                              Positioned(
+                                                  right: 20,
+                                                  bottom: 20,
+                                                  child: Image.asset(
+                                                    'assets/images/visa_logo.png',
+                                                    height: 20,
+                                                  )),
+                                              Positioned(
+                                                right: 6,
+                                                top: 10,
+                                                child: PopupMenuButton<String>(
+                                                  icon: const Icon(
+                                                    Icons.more_vert_rounded,
+                                                    color: Colors.white,
+                                                    size: 32,
+                                                  ),
+                                                  itemBuilder: (BuildContext
+                                                          context) =>
+                                                      <PopupMenuEntry<String>>[
+                                                    PopupMenuItem<String>(
+                                                      value: '1',
+                                                      child: const Text('Edit'),
+                                                      onTap: () {
+                                                        Navigator.of(context).push(
+                                                            MaterialPageRoute(
+                                                                builder: (context) =>
+                                                                    AddCardScreen(
+                                                                        card:
+                                                                            card)));
+                                                      },
+                                                    ),
+                                                    PopupMenuItem<String>(
+                                                      value: '2',
+                                                      child:
+                                                          const Text('Delete'),
+                                                      onTap: () {
+                                                        cardsCubit
+                                                            .removeCard(card);
+                                                      },
                                                     ),
                                                   ],
                                                 ),
-                                              ],
-                                            )),
-                                        Positioned(
-                                            right: 20,
-                                            bottom: 20,
-                                            child: Image.asset(
-                                              'assets/images/visa_logo.png',
-                                              height: 20,
-                                            )),
-                                        Positioned(
-                                          right: 6,
-                                          top: 10,
-                                          child: PopupMenuButton<String>(
-                                            icon: const Icon(
-                                              Icons.more_vert_rounded,
-                                              color: Colors.white,
-                                              size: 32,
-                                            ),
-                                            itemBuilder:
-                                                (BuildContext context) =>
-                                                    <PopupMenuEntry<String>>[
-                                              PopupMenuItem<String>(
-                                                value: '1',
-                                                child: const Text('Edit'),
-                                                onTap: () {
-                                                  Navigator.of(context).push(
-                                                      MaterialPageRoute(
-                                                          builder: (context) =>
-                                                              AddCardScreen(
-                                                                  card: card)));
-                                                },
-                                              ),
-                                              PopupMenuItem<String>(
-                                                value: '2',
-                                                child: const Text('Delete'),
-                                                onTap: () {
-                                                  cardsCubit.removeCard(card);
-                                                },
-                                              ),
+                                              )
                                             ],
                                           ),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ))
-                            .toList(),
-                      ),
+                                        ),
+                                      ))
+                                  .toList(),
+                            )
+                          : Container(),
                     ),
                   ],
                 ),
